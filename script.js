a1cf0fd17a57612bb3886defc33202cfbbe063da
GabrielaMendozal <mendozalizethg@gmail.com>
2022-06-30 13:35:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let animalimagen = document.querySelector ("#animal");
+
+console.log(animalimagen);
+
+
+function pickCat(){
+    animalimagen.src= "./icons/cat.png";
+}
+
+function pickDog(){
+    animalimagen.src= "./icons/dog.jpeg";
+}    
